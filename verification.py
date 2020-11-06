bfe1af02f602c3e8e2de24d49157f16d66d20b3d
belousov <belousovsergej56@gmail.com>
2020-11-06 23:52:45 +0300

    add anntation, add comment, remove old code

--- verification.py
+++ verification.py
@@ -1,7 +1,12 @@
+"""
+This model has methods for checking the letter entered by the user,
+checking the end of the game, checking for the letter in the secret word.
+"""
+
 import string
 
 
-def check_input_symbols(letters, input_list):
+def check_input_symbols(letters: str, input_list: list) -> bool:
     if len(letters) > 1 or len(letters) == 0:
         print("You should input a single letter")
         return True
@@ -15,7 +20,7 @@ def check_input_symbols(letters, input_list):
         return True
 
 
-def check_letters_in_secret_word(letters, word, answer, input_list):
+def check_letters_in_secret_word(letters: str, word: str, answer: str, input_list: list) -> bool:
     if letters not in word:
         print("No such letter in the word")
         return True
@@ -30,7 +35,7 @@ def check_letters_in_secret_word(letters, word, answer, input_list):
         return False
 
 
-def win_lost(word, answer, triers):
+def win_lost(word: str, answer: str, triers: int) -> bool:
     if answer == word:
         print(f"You guessed the word {word}!\nYou survived!")
         return True
